10871689e93dd6a7144b743af8a464877d9cedc4
w27y <mike@mikeshehata.com>
2020-11-27 15:11:07 -0800

    final static version

--- functions.js
+++ functions.js
@@ -1,1 +1,10 @@
+function scoreCalculate(wordCompletion) {
+if (wordCompletion === 9) {
+    score.textContent = "You Win, He gets to Live!!!";
+    console.log("score added");
+  }
 
+else {
+    console.log("Score is less than 9");
+}
+}
--- user.js
+++ user.js
@@ -1,6 +1,36 @@
 const form = document.getElementById('form');
 const log = document.getElementById('log');
-let resultLine = document.getElementById('resultLine');
+const score = document.getElementById('score');
+let resultA = document.getElementById('resultA');
+let resultB = document.getElementById('resultB');
+let resultC = document.getElementById('resultC');
+let resultD = document.getElementById('resultD');
+let resultE = document.getElementById('resultE');
+let resultF = document.getElementById('resultF');
+let resultG = document.getElementById('resultG');
+let resultH = document.getElementById('resultH');
+let resultI = document.getElementById('resultI');
+let resultJ = document.getElementById('resultJ');
+let resultK = document.getElementById('resultK');
+let resultL = document.getElementById('resultL');
+let resultM = document.getElementById('resultM');
+let resultN = document.getElementById('resultN');
+let resultO = document.getElementById('resultO');
+let resultO2 = document.getElementById('resultO2');
+let resultP = document.getElementById('resultP');
+let resultQ = document.getElementById('resultQ');
+let resultR = document.getElementById('resultR');
+let resultS = document.getElementById('resultS');
+let resultT = document.getElementById('resultT');
+let resultU = document.getElementById('resultU');
+let resultV = document.getElementById('resultV');
+let resultW = document.getElementById('resultW');
+let resultX = document.getElementById('resultX');
+let resultY = document.getElementById('resultY');
+let resultZ = document.getElementById('resultZ');
+let wordCompletion = 1;
+
+
 form.addEventListener('submit', logSubmit);
 
 function logSubmit(event) {
@@ -13,10 +43,66 @@ function logSubmit(event) {
 
         //Static Entry for Now
         function wordCheck(letterResult) {
-          if (letterResult === "R"){
+
+          if (letterResult === "R" && restultR != "R"){
             console.log("there is a match");
-            resultLine.textContent = "R _ _ _ _ _ _ _ _";
+            resultR.textContent = "R";
+            wordCompletion += 1;
+            scoreCalculate(wordCompletion);
             
+          }
+          else if (letterResult === "O"){
+            console.log("there is a match");
+            resultO.textContent = "O";
+            resultO2.textContent = "O";
+            wordCompletion += 1;
+            scoreCalculate(wordCompletion);
+          } 
+
+          else if (letterResult === "A"){
+            console.log("there is a match");
+            resultA.textContent = "A";
+            wordCompletion += 1;
+            scoreCalculate(wordCompletion);
+          } 
+
+          else if (letterResult === "D"){
+            console.log("there is a match");
+            resultD.textContent = "D";
+            wordCompletion += 1;
+            scoreCalculate(wordCompletion);
+          } 
+
+          else if (letterResult === "H"){
+            console.log("there is a match");
+            resultH.textContent = "H";
+            wordCompletion += 1;
+            scoreCalculate(wordCompletion);
+          } 
+
+          else if (letterResult === "U"){
+            console.log("there is a match");
+            resultU.textContent = "U";
+            wordCompletion += 1;
+            scoreCalculate(wordCompletion);
+          } 
+
+          else if (letterResult === "S"){
+            console.log("there is a match");
+            resultS.textContent = "S";
+            wordCompletion += 1;
+            scoreCalculate(wordCompletion);
+          } 
+
+          else if (letterResult === "E"){
+            console.log("there is a match");
+            resultE.textContent = "E";
+            wordCompletion += 1;
+            scoreCalculate(wordCompletion);
+          } 
+
+          else {
+            console.log("letter fail");
           }
 
         }
