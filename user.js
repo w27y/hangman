dacb4aeea020fea1c530f533a67e005f64a494f9
w27y <mike@mikeshehata.com>
2020-11-26 22:28:08 -0800

    Getting Closer to Static version

--- user.js
+++ user.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('form');
 const log = document.getElementById('log');
+let resultLine = document.getElementById('resultLine');
 form.addEventListener('submit', logSubmit);
 
 function logSubmit(event) {
@@ -8,6 +9,17 @@ function logSubmit(event) {
     log.textContent = `You entered: ${letterResult}`;
     event.preventDefault();
     console.log(letterUser.toUpperCase());
+    wordCheck(letterResult);
+
+        //Static Entry for Now
+        function wordCheck(letterResult) {
+          if (letterResult === "R"){
+            console.log("there is a match");
+            resultLine.textContent = "R _ _ _ _ _ _ _ _";
+            
+          }
+
+        }
     form.reset();
   }
   
