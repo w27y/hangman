f453fd5417f3494b32d9cd5c7bf943b5cb179f95
w27y <mike@mikeshehata.com>
2020-11-27 16:39:55 -0800

    Dynamic word checking, still testing

--- user.js
+++ user.js
@@ -1,36 +1,19 @@
-const form = document.getElementById('form');
-const log = document.getElementById('log');
-const score = document.getElementById('score');
-let resultA = document.getElementById('resultA');
-let resultB = document.getElementById('resultB');
-let resultC = document.getElementById('resultC');
-let resultD = document.getElementById('resultD');
-let resultE = document.getElementById('resultE');
-let resultF = document.getElementById('resultF');
-let resultG = document.getElementById('resultG');
-let resultH = document.getElementById('resultH');
-let resultI = document.getElementById('resultI');
-let resultJ = document.getElementById('resultJ');
-let resultK = document.getElementById('resultK');
-let resultL = document.getElementById('resultL');
-let resultM = document.getElementById('resultM');
-let resultN = document.getElementById('resultN');
-let resultO = document.getElementById('resultO');
-let resultO2 = document.getElementById('resultO2');
-let resultP = document.getElementById('resultP');
-let resultQ = document.getElementById('resultQ');
-let resultR = document.getElementById('resultR');
-let resultS = document.getElementById('resultS');
-let resultT = document.getElementById('resultT');
-let resultU = document.getElementById('resultU');
-let resultV = document.getElementById('resultV');
-let resultW = document.getElementById('resultW');
-let resultX = document.getElementById('resultX');
-let resultY = document.getElementById('resultY');
-let resultZ = document.getElementById('resultZ');
-let wordCompletion = 1;
+//Game Words Array
+const gameWordList = ["ROUDHOUSE", "FALCOR", "SAMSONITE", "KILOMETORY"];
 
+//Random Select a Word
+const gameWord = Math.floor(Math.random() * gameWordList.length);
+let selectedWord = gameWordList[gameWord];
+console.log("The word is:  " + selectedWord);
 
+//Create cell placeholders for each letter of the selected word
+let selectedWordLength = selectedWord.length;
+console.log("Word length is:  " + selectedWordLength);
+let htmlWord = document.getElementById('word');
+let placeHolder = "_ ";
+htmlWord.textContent = placeHolder.repeat(selectedWordLength);
+
+//User Input
 form.addEventListener('submit', logSubmit);
 
 function logSubmit(event) {
@@ -38,78 +21,24 @@ function logSubmit(event) {
     let letterResult = letterUser.toUpperCase();
     log.textContent = `You entered: ${letterResult}`;
     event.preventDefault();
-    console.log(letterUser.toUpperCase());
+    console.log("User typed:  " + letterResult);
     wordCheck(letterResult);
+}
 
-        //Static Entry for Now
-        function wordCheck(letterResult) {
-
-          if (letterResult === "R" && restultR != "R"){
-            console.log("there is a match");
-            resultR.textContent = "R";
-            wordCompletion += 1;
-            scoreCalculate(wordCompletion);
-            
-          }
-          else if (letterResult === "O"){
-            console.log("there is a match");
-            resultO.textContent = "O";
-            resultO2.textContent = "O";
-            wordCompletion += 1;
-            scoreCalculate(wordCompletion);
-          } 
-
-          else if (letterResult === "A"){
-            console.log("there is a match");
-            resultA.textContent = "A";
-            wordCompletion += 1;
-            scoreCalculate(wordCompletion);
-          } 
-
-          else if (letterResult === "D"){
-            console.log("there is a match");
-            resultD.textContent = "D";
-            wordCompletion += 1;
-            scoreCalculate(wordCompletion);
-          } 
-
-          else if (letterResult === "H"){
-            console.log("there is a match");
-            resultH.textContent = "H";
-            wordCompletion += 1;
-            scoreCalculate(wordCompletion);
-          } 
-
-          else if (letterResult === "U"){
-            console.log("there is a match");
-            resultU.textContent = "U";
-            wordCompletion += 1;
-            scoreCalculate(wordCompletion);
-          } 
-
-          else if (letterResult === "S"){
-            console.log("there is a match");
-            resultS.textContent = "S";
-            wordCompletion += 1;
-            scoreCalculate(wordCompletion);
-          } 
-
-          else if (letterResult === "E"){
-            console.log("there is a match");
-            resultE.textContent = "E";
-            wordCompletion += 1;
-            scoreCalculate(wordCompletion);
-          } 
-
-          else {
-            console.log("letter fail");
-          }
-
-        }
-    form.reset();
+//Check if letter matches part of word
+function wordCheck(letterResult) {
+  let validateLetter = selectedWord.includes(letterResult); 
+  console.log("validate log:  " + validateLetter);
+  if (validateLetter === true) {
+    console.log("Letter Matched");
+}
+  else {
+    console.log("Letter didn't match");
   }
-  
+}
 
+//Print letter if matched, draw body part if no match
 
+//If word is completed, move to next word and increase score
 
-  
+//If word is not completed after 5 fails, game over
