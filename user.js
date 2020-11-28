78ff5d9daf4aa8b12181b4478313c85c5bb59136
w27y <mike@mikeshehata.com>
2020-11-27 17:23:38 -0800

    more arrays

--- user.js
+++ user.js
@@ -1,56 +1,60 @@
-//Game Words Array
-const gameWordList = ["ROUDHOUSE", "FALCOR", "SAMSONITE", "KILOMETORY"];
-let failureArray = [];
+//Game Words, placeholder list, and tried letter list Array declarations
+    const gameWordList = ["ROUDHOUSE", "FALCOR", "SAMSONITE", "KILOMETORY"];
+    let listArray = [];
+    let failureArray = [];
 
 //Random Select a Word
-const gameWord = Math.floor(Math.random() * gameWordList.length);
-let selectedWord = gameWordList[gameWord];
-console.log("The word is:  " + selectedWord);
+    const gameWord = Math.floor(Math.random() * gameWordList.length);
+    let selectedWord = gameWordList[gameWord];
+    console.log("The word is:  " + selectedWord);
 
 //Create cell placeholders for each letter of the selected word
-let selectedWordLength = selectedWord.length;
-console.log("Word length is:  " + selectedWordLength);
-let htmlWord = document.getElementById('word');
-let placeHolder = "_ ";
-htmlWord.textContent = placeHolder.repeat(selectedWordLength);
+    let selectedWordLength = selectedWord.length;
+    console.log("Word length is:  " + selectedWordLength);
+    let htmlWord = document.getElementById('word');
+    let placeHolder = "_ ";
+    listArray.push(placeHolder.repeat(selectedWordLength));
+    htmlWord.textContent = listArray;
 
 //User Input
-form.addEventListener('submit', logSubmit);
+    form.addEventListener('submit', logSubmit);
 
-function logSubmit(event) {
-    let letterUser = document.getElementsByName("letter")[0].value;
-    let letterResult = letterUser.toUpperCase();
-    log.textContent = `You entered: ${letterResult}`;
-    event.preventDefault();
-    console.log("User typed:  " + letterResult);
-    wordCheck(letterResult);
-}
+    function logSubmit(event) {
+        let letterUser = document.getElementsByName("letter")[0].value;
+        let letterResult = letterUser.toUpperCase();
+        log.textContent = `You entered: ${letterResult}`;
+        event.preventDefault();
+        console.log("User typed:  " + letterResult);
+        wordCheck(letterResult);
+    }
 
 //Check if letter matches part of word
-function wordCheck(letterResult) {
-  let validateLetter = selectedWord.includes(letterResult); 
-  console.log("validate log:  " + validateLetter);
-  if (validateLetter === true) {
-    console.log("Letter Matched");
-    letterSuccess(letterResult);
-}
-  else {
-    console.log("Letter didn't match");
-    letterFail(letterResult);
-  }
-}
+    function wordCheck(letterResult) {
+      let validateLetter = selectedWord.includes(letterResult); 
+      console.log("validate log:  " + validateLetter);
+      if (validateLetter === true) {
+        console.log("Letter Matched");
+        letterSuccess(letterResult);
+    }
+      else {
+        console.log("Letter didn't match");
+        letterFail(letterResult);
+      }
+    }
 
-//Print letter if matched
-function letterSuccess(letterResult) {
-  
-}
+    //Print letter if matched
+    function letterSuccess(letterResult) {
+      let listLocation = gameWord.length(letterResult);
+      console.log(listLocation);
+      
+    }
 
 //draw body part if no match
-function letterFail(letterResult) {
-  let failLetters = document.getElementById('lettersTried');
-  failureArray.push(letterResult);
-  failLetters.textContent = failureArray;
-}
+    function letterFail(letterResult) {
+      let failLetters = document.getElementById('lettersTried');
+      failureArray.push(letterResult);
+      failLetters.textContent = failureArray;
+    }
 
 //If word is completed, move to next word and increase score
 
